842595a27ef6ad9272defe7b4463f7ca059114e3
heoegema <hollyoegema@gmail.com>
2020-04-30 15:44:23 -0400

    Allowing transactions to be broadcast on the network

--- backend/pubsub.py
+++ backend/pubsub.py
@@ -5,6 +5,7 @@ from pubnub.pnconfiguration import PNConfiguration
 from pubnub.callbacks import SubscribeCallback
 
 from backend.blockchain.block import Block
+from backend.wallet.transaction import Transaction
 
 subscribe_key =  'sub-c-75660ee4-8998-11ea-927a-2efbc014b69f'
 publish_key = 'pub-c-37f6471e-7c0e-4ec6-9244-01ab03b15326'
@@ -15,12 +16,14 @@ pnconfig.publish_key = publish_key
 
 CHANNELS = {
     'TEST': 'TEST',
-    'BLOCK': 'BLOCK'
+    'BLOCK': 'BLOCK',
+    'TRANSACTION': 'TRANSACTION'
 }
 
 class Listener(SubscribeCallback):
-    def __init__(self, blockchain):
+    def __init__(self, blockchain, transaction_pool):
         self.blockchain = blockchain
+        self.transaction_pool = transaction_pool
 
     def message(self, pubnub, message_object):
         print(f'\n-- Channel: {message_object.channel} | Message: {message_object.message}')
@@ -35,17 +38,20 @@ class Listener(SubscribeCallback):
                 print('\n -- Successfully replaced the local chain')
             except Exception as e:
                 print(f'\n -- Did not replace the chain {e}')
-
+        elif message_object.channel == CHANNELS['TRANSACTION']:
+            transaction = Transaction.from_json(message_object.message)
+            self.transaction_pool.set_transaction(transaction)
+            print('\n -- Set the new transaction in the transaction pool')
 
 class PubSub():
     """
     Handles the publish/subscribe layer of the application.
     Provides communciation between the nodes of the blockchain network.
     """
-    def __init__(self, blockchain):
+    def __init__(self, blockchain, transaction_pool):
         self.pubnub = PubNub(pnconfig)
         self.pubnub.subscribe().channels(CHANNELS.values()).execute()
-        self.pubnub.add_listener(Listener(blockchain))
+        self.pubnub.add_listener(Listener(blockchain, transaction_pool))
 
     def publish(self, channel, message):
         """
@@ -58,6 +64,12 @@ class PubSub():
         Broadcast a block object to all nodes.
         """
         self.publish(CHANNELS['BLOCK'], block.to_json())
+
+    def broadcast_transaction(self, transaction):
+        """
+        Broadcast a transaction to all nodes.
+        """
+        self.publish(CHANNELS['TRANSACTION'], transaction.to_json())
 
 def main():
     pubsub = PubSub()
--- backend/wallet/transaction.py
+++ backend/wallet/transaction.py
@@ -8,15 +8,15 @@ class Transaction:
     Document of an exchange in currency from a sender to one
     or more recipents.
     """
-    def __init__(self, sender_wallet, recipient, amount):
-        self.id = str(uuid.uuid4())[0:8]
-        self.output = self.create_output(
+    def __init__(self, sender_wallet=None, recipient=None, amount=None, id=None, output=None, input=None):
+        self.id = id or str(uuid.uuid4())[0:8]
+        self.output = output or self.create_output(
             sender_wallet,
             recipient,
             amount
         )
 
-        self.input = self.create_input(
+        self.input = input or self.create_input(
             sender_wallet,
             self.output
         )
@@ -64,6 +64,20 @@ class Transaction:
 
         self.input = self.create_input(sender_wallet, self.output)
 
+    def to_json(self):
+        """
+        Serialize the transaction.
+        """
+        return self.__dict__
+
+    @staticmethod
+    def from_json(transaction_json):
+        """
+        Deserialize a transaction's json representation back into a
+        Transaction instance
+        """
+        return Transaction(**transaction_json)
+
     @staticmethod
     def is_valid_transaction(transaction):
         """
@@ -85,6 +99,10 @@ class Transaction:
 def main():
     transaction = Transaction(Wallet(), 'recipient', 15)
     print(f'transaction.__dict__: {transaction.__dict__}')
+
+    transaction_json = transaction.to_json()
+    restored_transaction = Transaction.from_json(transaction_json)
+    print(f'restored_transaction.__dict__: {restored_transaction.__dict__}')
 
 if __name__ == '__main__':
     main()
